9bcbfd7f90d5fa8aeb40566c5c844dcf14b00a0a
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-03-14 19:41:11 +0500

    Add files via upload

---
+++ 32. Introduction to JavaScript/new.js
@@ -0,0 +1,3 @@
+console.log("Code is running")
+console.log("Code is in good position")
+console.log("Code is just looking like a wow")
---
+++ 32. Introduction to JavaScript/scrip.js
@@ -0,0 +1,8 @@
+alert("Hey i am Kashif Ali");
+
+console.log("Code is running")
+console.log("Code is in good position")
+console.log("Code is just looking like a wow")
+
+var a = prompt("Enter your number")
+console.log("Your number is " + a)
