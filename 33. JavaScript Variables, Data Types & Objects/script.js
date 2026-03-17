db8513ae57800bee3c0de97e9cd889b74d9de993
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-03-17 13:05:06 +0500

    Add files via upload

---
+++ 33. JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,43 @@
+console.log("Hello this is Kashif Ali")
+
+let a = 5;
+var b = 6;
+var c = " This is Kashif Ali";
+
+console.log(a + b + 9)
+
+{
+    let a = 50;
+    console.log(a)
+}
+
+console.log(a) 
+
+console.log(typeof a, typeof b, typeof c)
+
+const a1 = 55;
+// const a1 + 54; not allowed
+
+
+let x = "Kashif Ali";
+let y = 55;
+let z = 3.14;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r) 
+
+
+let Details = {
+    "Name": "Kashif Ali",
+    "Job Role": "Front End Developer",
+    "University": "Virtual University of Pakistan",
+}
+
+console.log(Details);
+
+Details.Salary = "10 lak";
+
+console.log(Details);
