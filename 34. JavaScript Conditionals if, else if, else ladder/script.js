7d8e3d603f15df763611bafcf5b7c3e0332855d1
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-03-18 13:41:41 +0500

    Add files via upload

---
+++ 34. JavaScript Conditionals if, else if, else ladder/script.js
@@ -0,0 +1,25 @@
+let age = 18;
+// let grace = 3;
+
+// console.log (age + grace);
+// console.log (age - grace);
+// console.log (age * grace);
+// console.log (age / grace);
+// console.log (age % grace);
+// console.log (age ** grace);
+
+// if ( (age + grace) > 18 ) {
+//     console.log("You can drive");
+// }
+
+// if ( age == 18 ) {
+//     console.log("You can drive");
+// }
+
+if ( age != 18 ) {
+    console.log("You can drive");
+}
+
+else{
+    console.log("You can not drive");
+}
