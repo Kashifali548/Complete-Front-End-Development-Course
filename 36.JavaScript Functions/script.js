39b01adcb1b30cf94a6d90a4a6b97d9cb2b037a7
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-03-26 09:04:30 +0500

    Add files via upload

---
+++ 36.JavaScript Functions/script.js
@@ -0,0 +1,19 @@
+function nice(name) {
+    console.log("Hi " + name + " Yor are very nice");
+    console.log("Hi " + name + " Yor are very good");
+    console.log("Hi " + name + " Yor are very kind");
+    console.log("Hi " + name + " Yor are very lovely");
+    console.log("Hi " + name + " Yor are very amazing");
+}
+
+nice("Kashif")
+// nice("Farasat");
+// nice("Hammad");
+
+function sum(a,b){
+    return a + b;
+}
+
+result = sum(3,5);
+
+console.log("The sum is: ", result)
