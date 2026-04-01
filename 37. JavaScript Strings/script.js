edf9db1cce8e30d473051792e53d629c03f0ed4e
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-04-01 09:13:46 +0500

    Add files via upload

---
+++ 37. JavaScript Strings/script.js
@@ -0,0 +1,29 @@
+console.log("Hello! my name is Kashif Ali");
+
+a = "Kashif";
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length)
+
+let real_name = "Kashif";
+let friend = "Hammad";
+
+console.log("My name is " + real_name + "My friend name is " + friend);
+
+// template literals
+console.log(`My name is ${real_name} and My friend name is ${friend}`);
+
+
+let b = "Kashif"
+
+console.log(b.toLowerCase());
+console.log(b.toUpperCase());
+console.log(b.length);
+console.log(b.slice(1, 3));
+
