c9c0d13bd72505557e66ab327c9fbb4fd3847773
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-04-20 18:49:22 +0500

    Add files via upload

---
+++ 40. JavaScript DOM - Children, Parent & Sibling Nodes/script.js
@@ -0,0 +1,9 @@
+console.log("Hello World!")
+
+// try this on chrome console
+
+// document.body
+// document.body.childNodes
+// document.body
+// document.body.childNodes[0]
+// document.body.childNodes[1]
